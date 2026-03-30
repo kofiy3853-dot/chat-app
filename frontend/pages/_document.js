8a93a39517526a0746a5e6260c6b5c9f95a482ab
kofiy3853.dot <kofiy3853@gmail.com>
2026-03-30 01:24:25 +0000

    fix(pwa): add crossorigin to manifest to fix 401 on vercel preview

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" crossOrigin="use-credentials" />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
         <meta name="theme-color" content="#0ea5e9" />
       </Head>
